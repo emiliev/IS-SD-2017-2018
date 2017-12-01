7729f4ce535f6d1ad00f26d9075e5016b0a2ee5d
Emil Iliev <emil_iliev1994@abv.bg>
2017-12-01 13:26:03 +0200

    add array'

--- Structures/Structures/main.cpp
+++ Structures/Structures/main.cpp
@@ -10,9 +10,16 @@
 #include "Stack.hpp"
 #include "List.hpp"
 #include "Queue.hpp"
+#include "DynamicArray.hpp"
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    
+    
+    DinamicArray<int> a;
+    a.addElement(5);
+    
+    
     return 0;
 }
